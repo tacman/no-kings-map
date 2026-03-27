8793219ef0e9b863a5c997075e22160812be6686
Tac Tacelosky <tacman@gmail.com>
2026-03-27 17:22:52 -0400

    fix spots

--- spots.js
+++ spots.js
@@ -10,30 +10,30 @@ window.PARKING_SPOTS = [
   },
   {
     id: 2,
+    name: "Western Style storefront #11961 Lee Hwy",
+    address: "11961 Lee Hwy, Sperryville, VA 22740",
+    notes: "Frontage parking on Lee Hwy.",
+    image: "./images/2.jpg",
+    mode: "address",
+    value: "11961 Lee Hwy, Sperryville, VA 22740"
+  },
+  {
+    id: 3,
     name: "Next to Sperryville post office",
     address: "43 Main St., Sperryville, VA 22740",
     notes: "On either side of Cheri Woodard Realty/Thornton Art building.",
-    image: "./images/2.jpg",
+    image: "./images/3.jpg",
     mode: "address",
     value: "43 Main St., Sperryville, VA 22740"
   },
   {
-    id: 3,
+    id: 4,
     name: "Between Black Bear Manor and fence",
     address: "3717 Sperryville Pike, Sperryville, VA 22740",
     notes: "Hopkins property rear parking area.",
-    image: "./images/3.jpg",
-    mode: "address",
-    value: "3717 Sperryville Pike, Sperryville, VA 22740"
-  },
-  {
-    id: 4,
-    name: "Western Style storefront #11961 Lee Hwy",
-    address: "11961 Lee Hwy, Sperryville, VA 22740",
-    notes: "Frontage parking on Lee Hwy.",
     image: "./images/4.jpg",
     mode: "address",
-    value: "11961 Lee Hwy, Sperryville, VA 22740"
+    value: "3717 Sperryville Pike, Sperryville, VA 22740"
   },
   {
     id: 5,
