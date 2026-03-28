0a7a444d05a12252e1069e4af65290067a49ba3d
Tac Tacelosky <tacman@gmail.com>
2026-03-28 04:49:00 -0400

    remote sanford guide, etc.

--- spots.js
+++ spots.js
@@ -3,7 +3,7 @@ window.PARKING_SPOTS = [
     id: 1,
     name: "#11771 Lee Hwy (shuttle to site)",
     address: "11771 Lee Hwy, Sperryville, VA 22740",
-    notes: "Rally & parking. Main event location.",
+    notes: "",
     image: "./images/1.jpg",
     mode: "address",
     value: "11771 Lee Hwy, Sperryville, VA 22740"
@@ -12,7 +12,7 @@ window.PARKING_SPOTS = [
     id: 2,
     name: "Western Style storefront #11961 Lee Hwy",
     address: "11961 Lee Hwy, Sperryville, VA 22740",
-    notes: "Frontage parking on Lee Hwy.",
+    notes: "",
     image: "./images/2.jpg",
     mode: "address",
     value: "11961 Lee Hwy, Sperryville, VA 22740"
@@ -21,7 +21,7 @@ window.PARKING_SPOTS = [
     id: 3,
     name: "Next to Sperryville post office",
     address: "43 Main St., Sperryville, VA 22740",
-    notes: "On either side of Cheri Woodard Realty/Thornton Art building.",
+    notes: "",
     image: "./images/3.jpg",
     mode: "address",
     value: "43 Main St., Sperryville, VA 22740"
@@ -30,7 +30,7 @@ window.PARKING_SPOTS = [
     id: 4,
     name: "Between Black Bear Manor and fence",
     address: "3717 Sperryville Pike, Sperryville, VA 22740",
-    notes: "Hopkins property rear parking area.",
+    notes: "",
     image: "./images/4.jpg",
     mode: "address",
     value: "3717 Sperryville Pike, Sperryville, VA 22740"
@@ -39,7 +39,7 @@ window.PARKING_SPOTS = [
     id: 5,
     name: "Yard behind green house",
     address: "",
-    notes: "Public parking near post office.",
+    notes: "",
     image: "./images/5.jpg",
     mode: "address",
     value: ""
